7eb2882fd425d4e3fb53bf2a68f7305f1c0c1dda
Ecruz2026 <ecruz9@my.nl.edu>
2024-11-03 23:38:43 -0600

    Update team.assignmentm6.py

--- team.assignmentm6.py
+++ team.assignmentm6.py
@@ -1,3 +1,15 @@
+# Syeda Khunza Fatima
+# Emmanuel Cruz
+# oct 29, 2024
+
+# generate the random number from 1-25
+# loop to allow user to guess
+# prompt the user to enter a guess
+# covert users input to integers
+# guess the higher number
+# if you guessed it right then you won
+
+
 import random
 
 # Step 1: generate the random number from 1-25
@@ -10,7 +22,7 @@ print("I have generated a random number between 1 and 25. Try to guess it!")
 while True:
     # Step 3: prompt the user to enter a guess
     user_input = input("Enter your guess: ")
-   
+    
     # Step 4: convert user's input to integers
     try:
         user_guess = int(user_input)
